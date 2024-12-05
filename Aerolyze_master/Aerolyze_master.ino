249ca77e38db602321489bbc4f104569f81f5901
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-12-05 18:06:22 +0700

    feat: initial commit

---
+++ Aerolyze_master/Aerolyze_master.ino
@@ -0,0 +1,62 @@
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH_TOKEN ""
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+BlynkTimer timer;
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "SSID";
+char pass[] = "";
+
+#define MQ2 34
+#define GREEN 18
+#define RED 19
+int sensorValue = 0;
+boolean state = false;
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(MQ2, INPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  timer.setInterval(1000L, sendUptime);
+}
+
+void sendUptime()
+{
+  
+  sensorValue = analogRead(MQ2);
+  Blynk.virtualWrite(V1, sensorValue);
+  Serial.println(sensorValue);
+
+  if (sensorValue > 600)
+  {
+    // Blynk.notify("Gas Detected!");
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(RED, LOW);
+  }
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
