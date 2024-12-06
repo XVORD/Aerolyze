29e0d1b8ba4e66671029bd986028eaac78fd8107
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-12-06 15:01:38 +0700

    feat: add mesh

--- Aerolyze_master/Aerolyze_master.ino
+++ Aerolyze_master/Aerolyze_master.ino
@@ -1,68 +1,87 @@
-#define BLYNK_PRINT Serial
 #include <painlessMesh.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-BlynkTimer timer;
+#include <painlessMesh.h>
+
+painlessMesh mesh;
 
 #define MESH_SSID "MeshNetworkSSID"
 #define MESH_PASSWORD "password123"
 #define MESH_PORT 5555
-painlessMesh mesh;
 
-#define BLYNK_TEMPLATE_ID ""
-#define BLYNK_TEMPLATE_NAME ""
-#define BLYNK_AUTH_TOKEN ""
+QueueHandle_t MQ2Queue;
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = BLYNK_AUTH_TOKEN;
+void sendUptime();
+void taskMQ2(void *pvParameters);
+void receivedCallback(uint32_t from, String &msg);
+void sendMessage();
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "SSID";
-char pass[] = "";
+#define MESH_PREFIX "meshNetwork"
+#define MESH_PASSWORD "meshpassword"
+#define MESH_PORT 5555
 
 #define MQ2 34
 #define GREEN 18
 #define RED 19
+
 int sensorValue = 0;
 boolean state = false;
 
-void setup()
-{
-  // Debug console
+struct GasData {
+  float gasvalue;
+};
+
+void setup() {
   Serial.begin(115200);
-  Blynk.begin(auth, ssid, pass);
+
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+  mesh.onReceive(&receivedCallback);
+
   pinMode(MQ2, INPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(RED, OUTPUT);
-  timer.setInterval(1000L, sendUptime);
+
+  MQ2Queue = xQueueCreate(5, sizeof(GasData));
 }
 
-void sendUptime()
-{
-  
+void loop() {
+  mesh.update();
+}
+
+void sendUptime() {
   sensorValue = analogRead(MQ2);
-  Blynk.virtualWrite(V1, sensorValue);
   Serial.println(sensorValue);
 
-  if (sensorValue > 600)
-  {
-    // Blynk.notify("Gas Detected!");
+  if (sensorValue > 600) {
     digitalWrite(GREEN, LOW);
     digitalWrite(RED, HIGH);
-  }
-
-  else
-  {
+  } else {
     digitalWrite(GREEN, HIGH);
     digitalWrite(RED, LOW);
   }
+
+  if (xQueueSend(MQ2Queue, &sensorValue, pdMS_TO_TICKS(10)) != pdPASS) {
+    Serial.println("Failed to send MQ2 data to queue");
+  } else {
+    Serial.println("MQ2 data sent to queue successfully!");
+  }
+
+  sendMessage();
 }
 
-void loop()
-{
-  Blynk.run();
-  timer.run();
+void sendMessage() {
+  String msg = String(sensorValue);
+  while (1) {
+    // Menerima data dari queue
+    if (xQueueReceive(MQ2Queue, &sensorValue, portMAX_DELAY) == pdTRUE) { 
+      mesh.sendBroadcast(msg);
+      Serial.printf("Broadcast message: %s\n", msg.c_str());
+    }
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void receivedCallback(uint32_t from, String &msg) {
+  Serial.printf("Received from %u: %s\n", from, msg.c_str());
 }
