bd1603d91d21134c0ce2339dfb8067006c496d51
rizkyAlya <aliyahrizky05@gmail.com>
2024-12-08 18:21:47 +0700

    add priority inversion in root code

--- Aerolyze_master/Aerolyze_master.ino
+++ Aerolyze_master/Aerolyze_master.ino
@@ -1,87 +1,166 @@
 #include <painlessMesh.h>
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <painlessMesh.h>
+#include <HTTPClient.h>
+#include <UrlEncode.h>
+#include <time.h>
+
+#define LED_PIN 5
+#define BUZZER_PIN 4
+
+#define MESH_SSID "Mesh_Kelompok_10"
+#define MESH_PASSWORD "Mesh_Kelompok_10"
+#define MESH_PORT 5555
 
 painlessMesh mesh;
 
-#define MESH_SSID "MeshNetworkSSID"
-#define MESH_PASSWORD "password123"
-#define MESH_PORT 5555
+// Kredensial WiFi
+const char* ssid = "";
+const char* password = "";
 
-QueueHandle_t MQ2Queue;
+// Dapatkan dari: https://www.electronicwings.com/esp32/send-a-whatsapp-message-using-esp32
+String MobileNumber = "";
+String APIKey = "";
 
-void sendUptime();
-void taskMQ2(void *pvParameters);
-void receivedCallback(uint32_t from, String &msg);
-void sendMessage();
+// Deklarasi fungsi
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info);  // Fungsi callback saat WiFi terhubung
+void taskHandleGas(void* pvParameters);                              // Task untuk mengirimkan notifikasi serta menghidupkan alarm
+void activateAlarm();                                                // Fungsi untuk menghidupkan LED dan buzzer
+void sendMessage(String message);                                    // Fungsi untuk mengirimkan notifikasi peringatan ke WhatsApp
+void vTimeoutCallback(TimerHandle_t xTimer);                         // Fungsi callback saat timer selesai
+void taskHandleSafe(void* pvParameters);                             // Fungsi untuk mematikan LED dan buzzer
+void receivedCallback(uint32_t from, String& msg);                   // Fungsi callback untuk menerima pesan mesh
 
-#define MESH_PREFIX "meshNetwork"
-#define MESH_PASSWORD "meshpassword"
-#define MESH_PORT 5555
+const int timeoutPeriod = 5000;      // Periode timeout = 60 detik (5 detik untuk percobaan)
+int gasValue = 0;                    // Menyimpan data yang diterima dari leaf node dalam bentuk integer
+TaskHandle_t xTaskHandleGas = NULL;  // Handle untuk taskHandleGas
+TimerHandle_t xTimeoutTimer;         // Timeout timer handle
+TaskHandle_t xTaskHandleSafe;        // Handle untuk taskHandleSafe
 
-#define MQ2 34
-#define GREEN 18
-#define RED 19
-
-int sensorValue = 0;
-boolean state = false;
-
-struct GasData {
-  float gasvalue;
-};
+bool alarmActivated = false;    // Status alarm saat ini
+bool notificationSent = false;  // Status notifikasi saat ini
+int previousGasState = 0;       // 0 untuk normal, 1 untuk berbahaya
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);  // Memulai komunikasi serial
 
-  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
-  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
-  mesh.onReceive(&receivedCallback);
+  pinMode(LED_PIN, OUTPUT);     // Inisialisasi LED_PIN sebagai output
+  pinMode(BUZZER_PIN, OUTPUT);  // Inisialisasi BUZZER_PIN sebagai output
 
-  pinMode(MQ2, INPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(RED, OUTPUT);
+  // Mendaftarkan WiFi event yang digunakan
+  WiFi.onEvent(WiFiStationConnected, ARDUINO_EVENT_WIFI_STA_CONNECTED);
 
-  MQ2Queue = xQueueCreate(5, sizeof(GasData));
+  // Memulai koneksi WiFi
+  Serial.println("Menghubungkan ke WiFi dengan SSID: " + String(ssid));
+  WiFi.begin(ssid, password);
+
+  // Inisialisasi Mesh
+  mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
+  mesh.onReceive(receivedCallback);
+
+  // Membuat timer
+  xTimeoutTimer = xTimerCreate("Timeout Timer", timeoutPeriod, pdFALSE, 0, vTimeoutCallback);
+
+  xTaskCreate(taskHandleGas, "Handle Gas", 4096, NULL, 1, &xTaskHandleGas);  // Membuat tugas untuk handle gas berbahaya
 }
 
 void loop() {
   mesh.update();
 }
 
-void sendUptime() {
-  sensorValue = analogRead(MQ2);
-  Serial.println(sensorValue);
-
-  if (sensorValue > 600) {
-    digitalWrite(GREEN, LOW);
-    digitalWrite(RED, HIGH);
-  } else {
-    digitalWrite(GREEN, HIGH);
-    digitalWrite(RED, LOW);
-  }
-
-  if (xQueueSend(MQ2Queue, &sensorValue, pdMS_TO_TICKS(10)) != pdPASS) {
-    Serial.println("Failed to send MQ2 data to queue");
-  } else {
-    Serial.println("MQ2 data sent to queue successfully!");
-  }
-
-  sendMessage();
+// WiFi Event untuk mencetak informasi bahwa koneksi WiFi berhasil
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Berhasil terhubung dengan " + String(ssid));
 }
 
-void sendMessage() {
-  String msg = String(sensorValue);
-  while (1) {
-    // Menerima data dari queue
-    if (xQueueReceive(MQ2Queue, &sensorValue, portMAX_DELAY) == pdTRUE) { 
-      mesh.sendBroadcast(msg);
-      Serial.printf("Broadcast message: %s\n", msg.c_str());
+void receivedCallback(uint32_t from, String& msg) {
+  String gasStr = msg.substring(0, 5);  // Mengambil data yang diterima dari leaf node
+  gasValue = atoi(gasStr.c_str());      // Konversi nilai gas ke integer
+
+  // Menerapkan priority inversion
+  if (gasValue > 1300) {
+    Serial.printf("Pesan diterima dari Node ID %u: %s\n", from, msg.c_str());
+    vTaskPrioritySet(xTaskHandleGas, 2);  // Prioritas tugas handle gas dinaikkan
+  } else {
+    vTaskPrioritySet(xTaskHandleGas, 1);  // Prioritas tugas handle gas dikembalikan ke awal
+  }
+}
+
+// Tugas untuk handle gas berbahaya
+void taskHandleGas(void* pvParameters) {
+  while (true) {
+    // Jika gas dalam kondisi berbahaya
+    if (gasValue > 1200) {
+      if (previousGasState == 0) {                 // Jika sebelumnya kondisi normal
+        activateAlarm();                           // Aktifkan LED dan buzzer
+        sendMessage("Gas berbahaya terdeteksi!");  // Kirim notifikasi
+        notificationSent = true;
+        alarmActivated = true;
+        previousGasState = 1;  // Ubah status menjadi berbahaya
+      }
+    } else {
+      // Jika gas kembali normal
+      if (previousGasState == 1) {
+        if (!xTimerIsTimerActive(xTimeoutTimer)) {  // Jika timer tidak aktif
+          xTimerStart(xTimeoutTimer, 0);            // Memulai timer
+          Serial.println("\nMemulai timer untuk mematikan alarm!");
+        }                                                // Jika sebelumnya dalam kondisi berbahaya
+        sendMessage("Gas berbahaya tidak terdeteksi!");  // Kirim notifikasi kembali normal
+        notificationSent = false;
+        alarmActivated = false;
+        previousGasState = 0;  // Ubah status menjadi normal
+      }
     }
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);  // Delay untuk efisiensi loop
   }
 }
 
-void receivedCallback(uint32_t from, String &msg) {
-  Serial.printf("Received from %u: %s\n", from, msg.c_str());
+
+// Fungsi untuk menghidupkan LED dan buzzer
+void activateAlarm() {
+  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(BUZZER_PIN, HIGH);
+  Serial.println("ALARM: Gas berbahaya terdeteksi!");
+}
+
+// Fungsi untuk mengirimkan notifikasi peringatan ke WhatsApp
+void sendMessage(String message) {
+  String url = "https://api.callmebot.com/whatsapp.php?phone=" + MobileNumber + "&text=" + urlEncode(message) + "&apikey=" + APIKey;
+  HTTPClient http;
+  http.begin(url, true);
+
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.setTimeout(5000);  // Timeout 5 detik
+
+  int httpResponseCode = http.POST(url);
+  //delay(2000);
+  if (httpResponseCode == 200) {
+    Serial.println("Berhasil mengirimkan notifikasi");
+  } else {
+    Serial.println("Terjadi kesalahan saat mengirimkan notifikasi");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+}
+
+// Fungsi callback milik timer
+void vTimeoutCallback(TimerHandle_t xTimer) {
+  if (xTaskHandleSafe == NULL) {
+    xTaskCreate(taskHandleSafe, "Handle Safe", 4096, NULL, 1, &xTaskHandleSafe);
+  }
+}
+
+// Tugas untuk mematikan alarm
+void taskHandleSafe(void* pvParameters) {
+  Serial.println("Gas berbahaya tidak terdeteksi. Mematikan alarm!");
+
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(BUZZER_PIN, LOW);
+
+  sendMessage("Gas berbahaya sudah tidak terdeteksi!");
+
+  // Menghapus task saat selesai
+  xTaskHandleSafe = NULL;
+  vTaskDelete(NULL);
 }
--- Aerolyze_slave/Aerolyze_slave.ino
+++ Aerolyze_slave/Aerolyze_slave.ino
@@ -1,153 +1,90 @@
 #include <painlessMesh.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
-#include <UrlEncode.h>
-#include <time.h>
-
-#define LED_PIN 2
-
-#define MESH_SSID "MeshNetworkSSID"
-#define MESH_PASSWORD "password123"
-#define MESH_PORT 5555
+#include <WiFiClient.h>
 
 painlessMesh mesh;
 
-// Kredensial WiFi
-const char* ssid = "";
-const char* password = "";
+#define MESH_SSID "Mesh_Kelompok_10"
+#define MESH_PASSWORD "Mesh_Kelompok_10"
+#define MESH_PORT 5555
 
-// Dapatkan dari: https://www.electronicwings.com/esp32/send-a-whatsapp-message-using-esp32
-String MobileNumber = "";
-String APIKey = "";
+QueueHandle_t MQ2Queue;
 
-// Deklarasi fungsi
-void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info);  // Fungsi callback saat WiFi terhubung
-void taskHandleGasDetected(void* pvParameters);                      // Task untuk mengirimkan notifikasi serta menghidupkan alarm
-void activateAlarm();                                                // Fungsi untuk menghidupkan LED dan buzzer
-void sendMessage(String message);                                    // Fungsi untuk mengirimkan notifikasi peringatan ke WhatsApp
-void vTimeoutCallback(TimerHandle_t xTimer);                         // Fungsi callback saat timer selesai
-void taskHandleSafe(void* pvParameters);
-void receivedCallback(uint32_t from, String &msg); // Tambahan: Callback untuk menerima pesan mesh
+void MQ2task(void* pvParameters);
+void taskMQ2(void* pvParameters);
+void receivedCallback(uint32_t from, String& msg);
+void MeshTask(void* pvParameters);
 
-const int timeoutPeriod = 5000;  // Periode timeout = 60 detik (5 detik untuk percobaan)
-TimerHandle_t xTimeoutTimer;     // Timeout timer handle
-TaskHandle_t xTaskHandleSafe;    // Handle untuk taskHandleSafe
+#define MQ2 34
+#define GREEN 18
+#define RED 19
 
-// Flag untuk memastikan notifikasi dan alarm hanya sekali
-bool notificationSent = false;
-bool alarmActivated = false;
+struct GasData {
+  float gasvalue;
+};
 
 void setup() {
-  Serial.begin(115200);  // Memulai komunikasi serial
+  Serial.begin(115200);
 
-  pinMode(LED_PIN, OUTPUT);  // Inisialisasi LED_PIN sebagai output
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
+  mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
+  mesh.onReceive(&receivedCallback);
 
-  // Mendaftarkan WiFi event yang digunakan
-  WiFi.onEvent(WiFiStationConnected, ARDUINO_EVENT_WIFI_STA_CONNECTED);
+  pinMode(MQ2, INPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
 
-  // Memulai koneksi WiFi
-  Serial.println("Menghubungkan ke WiFi dengan SSID: " + String(ssid));
-  WiFi.begin(ssid, password);
+  MQ2Queue = xQueueCreate(5, sizeof(GasData));
+  if (MQ2Queue == NULL) {
+    Serial.println("Failed to create MQ2 queue");
+    while (1)
+      ;
+  }
 
-  // Inisialisasi Mesh
-  mesh.init(MESH_SSID, MESH_PASSWORD, &MESH_PORT);
-  mesh.onReceive(receivedCallback);
-
-  // Membuat timer
-  xTimeoutTimer = xTimerCreate("Timeout Timer", timeoutPeriod, pdFALSE, 0, vTimeoutCallback);
-
-  xTaskCreate(taskHandleGas, "Handle Gas", 4096, NULL, 1, NULL);  // Membuat tugas untuk handle gas berbahaya
+  xTaskCreate(MQ2task, "MQ2 Task", 4096, NULL, 1, NULL);
+  xTaskCreate(MeshTask, "Mesh Task", 4096, NULL, 1, NULL);
 }
 
 void loop() {
-  mesh.update();// Tidak digunakan
+  mesh.update();
 }
 
-// WiFi Event untuk mencetak informasi bahwa koneksi WiFi berhasil
-void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info) {
-  Serial.println("Berhasil terhubung dengan " + String(ssid));
+void MQ2task(void* pvParameters) {
+  while (1) {
+    GasData gasData;
+    gasData.gasvalue = analogRead(MQ2);
+    Serial.printf("MQ2 Sensor Value: %.2f\n", gasData.gasvalue);
+
+    if (gasData.gasvalue > 1200) {
+      digitalWrite(GREEN, LOW);
+      digitalWrite(RED, HIGH);
+    } else {
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(RED, LOW);
+    }
+
+    if (xQueueSend(MQ2Queue, &gasData, pdMS_TO_TICKS(10)) != pdPASS) {
+      Serial.println("Failed to send MQ2 data to queue");
+    } else {
+      Serial.println("MQ2 data sent to queue successfully!");
+    }
+
+    vTaskDelay(5000 / portTICK_PERIOD_MS);  // Delay to prevent overwhelming the task
+  }
+}
+
+void MeshTask(void* pvParameters) {
+  GasData receivedData;
+  while (1) {
+    if (xQueueReceive(MQ2Queue, &receivedData, portMAX_DELAY) == pdTRUE) {
+        String msg = String(receivedData.gasvalue);
+        mesh.sendBroadcast(msg);
+        Serial.printf("Broadcast message: %s\n", msg.c_str());
+    }
+    vTaskDelay(5000 / portTICK_PERIOD_MS);  // Delay to control broadcast frequency
+  }
 }
 
 void receivedCallback(uint32_t from, String &msg) {
-  Serial.printf("Pesan diterima dari Node ID %u: %s\n", from, msg.c_str());
-}
-
-// Tambahan: Fungsi untuk mengirim pesan melalui mesh
-void sendMeshMessage(String message) {
-  mesh.sendBroadcast(message);
-  Serial.println("Pesan dikirim ke mesh: " + message);
-}
-
-// Tugas untuk handle gas berbahaya
-void taskHandleGas(void* pvParameters) {
-  while (true) {
-    if (Serial.available() > 0) {
-      String input = Serial.readStringUntil('\n');
-
-      if (input == "ada") {  // Untuk percobaan, nanti menggunakan data yang diterima dari root
-        if (!notificationSent && !alarmActivated) {
-          notificationSent = true;
-          alarmActivated = true;
-
-          activateAlarm();
-          sendMessage("Gas berbahaya terdeteksi!");
-        }
-      } else {
-        notificationSent = false;
-        alarmActivated = false;
-
-        if (!xTimerIsTimerActive(xTimeoutTimer)) {  // Jika timer tidak aktif
-          xTimerStart(xTimeoutTimer, 0);            // Memulai timer
-          Serial.println("\nMemulai timer untuk mematikan alarm!");
-        }
-      }
-    }
-  }
-}
-
-// Fungsi untuk menghidupkan LED dan buzzer
-void activateAlarm() {
-  digitalWrite(LED_PIN, HIGH);
-  //digitalWrite(BUZZER_PIN, HIGH);
-  Serial.println("ALARM: Gas berbahaya terdeteksi!");
-}
-
-// Fungsi untuk mengirimkan notifikasi peringatan ke WhatsApp
-void sendMessage(String message) {
-  String url = "https://api.callmebot.com/whatsapp.php?phone=" + MobileNumber + "&apikey=" + APIKey + "&text=" + urlEncode(message);
-  HTTPClient http;
-  http.begin(url);
-
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  int httpResponseCode = http.POST(url);
-  if (httpResponseCode == 200) {
-    Serial.println("Berhasil mengirimkan notifikasi");
-  } else {
-    Serial.println("Terjadi kesalahan saat mengirimkan notifikasi");
-    Serial.print("HTTP response code: ");
-    Serial.println(httpResponseCode);
-  }
-  http.end();
-}
-
-// Fungsi callback milik timer
-void vTimeoutCallback(TimerHandle_t xTimer) {
-  if (xTaskHandleSafe == NULL) {
-    xTaskCreate(taskHandleSafe, "Handle Safe", 4096, NULL, 1, &xTaskHandleSafe);
-  }
-}
-
-// Tugas untuk mematikan alarm
-void taskHandleSafe(void* pvParameters) {
-  Serial.println("Gas berbahaya tidak terdeteksi. Mematikan alarm!");
-
-  digitalWrite(LED_PIN, LOW);
-  // digitalWrite(BUZZER_PIN, LOW);
-
-  sendMessage("Gas berbahaya sudah tidak terdeteksi!");
-
-  // Menghapus task saat selesai
-  xTaskHandleSafe = NULL;
-  vTaskDelete(NULL);
+  Serial.printf("Received from %u: %s\n", from, msg.c_str());
 }
